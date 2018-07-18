9bd4daff908bdbed7b63f12bcc90696dc3e197a7
soysebassss <40040414+soysebassss@users.noreply.github.com>
2018-07-18 18:29:16 -0400

    proyect

---
+++ application/views/navBar.php
@@ -0,0 +1,21 @@
+<div class="products">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1>hola</h1>
+           
+            
+        </div>
+        <div class="col">
+          <h1>hola</h1>
+           
+            
+        </div>
+        <div class="col">
+          <h1>hola</h1>
+           
+            
+        </div>
+      </div>
+    </div>
+  </div>
