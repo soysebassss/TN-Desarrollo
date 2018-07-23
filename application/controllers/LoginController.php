3c85da168a467a14b71047d1086ac7a896f3ab0b
Jav1err <41394161+Jav1err@users.noreply.github.com>
2018-07-23 17:47:17 -0400

    corrección

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -4,7 +4,7 @@ class LoginController extends CI_Controller {
 
 	public function __construct(){
 		parent::__construct();
-		$this->load->model('Usuario_model','user',true);
+		$this->load->model('User_model','user',true);
 		$this->load->library('session');
 	}
 
