5a73915fcaca53632fcdcf0d4d9e34e07c38ad46
soysebassss <40040414+soysebassss@users.noreply.github.com>
2018-08-03 08:27:24 -0400

    revisar

--- application/models/FormaPago_model.php
+++ application/models/FormaPago_model.php
@@ -7,8 +7,17 @@ class FormaPago_model extends CI_Model {
 		'for_abono' => 0,
 		'for_fecha' => '',
 		'for_numero' => 0,
-		'for_tipo' => 0
+		'for_tipo' => 1,
+      'for_clave' => 1
 	);
+   private $_columnsFac = array(
+      'for_id' => 0,
+      'for_fecha' => '',
+      'for_numero' => 0,
+      'for_tipo' => 2,
+      'for_banco' => "",
+      'for_clave' => 2
+   );
 	protected static $_table = 'FormaPago';
 
 	public function __construct(){
@@ -45,10 +54,26 @@ class FormaPago_model extends CI_Model {
       }
       return $emptyCollumn;
    }
+
+   public function validateFac() {
+      $emptyCollumn = array();
+      foreach ($this->_columnsFac as $key => $value) {
+         if ((is_null($value) || empty(str_replace(' ', "", $value))) && in_array($key, $this->getRequired())) {
+            $emptyCollumn[] = $key;
+         }
+      }
+      return $emptyCollumn;
+   }
    
    public function setColumns($row = null) {
       foreach ($row as $key => $value) {
          $this->_columns[$key] = $value;
+      }
+   }
+
+   public function setColumnsFac($row = null) {
+      foreach ($row as $key => $value) {
+         $this->_columnsFac[$key] = $value;
       }
    }
    
@@ -76,6 +101,11 @@ class FormaPago_model extends CI_Model {
       $formaPago->setColumns($row);
       return $formaPago;
    }
+   public function createFac($row) {
+      $formaPago = new FormaPago_model();
+      $formaPago->setColumnsFac($row);
+      return $formaPago;
+   }
    
    public function save() {
       try {
@@ -86,6 +116,20 @@ class FormaPago_model extends CI_Model {
          } else {
             $this->db->where('for_id', $this->_columns['for_id']);
             $this->db->update(self::$_table, $this->_columns);
+         }
+      } catch (Exception $e) {
+         echo "se produjo una excepcion del tipo".$e->getMessage();
+      }
+   }
+   public function saveFac() {
+      try {
+         $this->load->database();
+         if ($this->_columnsFac['for_id'] == 0 || is_null($this->_columnsFac['for_id'])) {
+            $this->db->insert(self::$_table, $this->_columnsFac);
+            $this->_columnsFac['for_id'] = $this->db->insert_id();
+         } else {
+            $this->db->where('for_id', $this->_columnsFac['for_id']);
+            $this->db->update(self::$_table, $this->_columnsFac);
          }
       } catch (Exception $e) {
          echo "se produjo una excepcion del tipo".$e->getMessage();
